bf0618373f1d9ceaf5fa704a6b2ed3804b94a6da
Brandon <baggatbt@gmail.com>
2020-02-22 09:24:41 -0500

    exercise two

---
+++ JS_101/lesson_2/Isnt_It_Odd.js
@@ -0,0 +1,7 @@
+function isOdd(number) {
+
+	return Math.abs(number) % 2 === 1;
+	
+}
+console.log(isOdd(2));
+console.log(isOdd(5));
---
+++ JS_101/lesson_2/Odd_Numbers.js
@@ -0,0 +1,14 @@
+for (i = 0; i < 100; i++) {
+	if (i % 2 != 0) {
+		let number = i;
+		console.log(number);
+	}
+}
+
+
+
+/*Simplest form
+for (let number = 1; number < 100; number += 2) {
+  console.log(number);
+}
+*/
